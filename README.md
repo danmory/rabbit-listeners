aad7e17534bed3d46a09db77d44f222f3fb6fbc2
Danila Moriakov <d.moriakov@gmail.com>
2023-02-01 14:05:50 +0300

    program and docs

---
+++ README.md
@@ -0,0 +1,17 @@
+# RabbitMQ listeners
+
+## Description
+
+The program starts to listen specified queues
+and outputs messages to stdout or file.
+
+Each queue listener starts in a different process.
+
+All configuration is in *config.yaml*.
+
+## Usage
+
+1. Add your configuration to *config.yaml*.
+2. Run
+
+    ``$ python main.py``
---
+++ main.py
@@ -0,0 +1,62 @@
+import multiprocessing
+import pika
+import yaml
+
+
+def read_config():
+  with open("config.yaml", "r") as f:
+    config = yaml.safe_load(f)
+  return config
+
+
+def on_message(out_file=None):
+  def on_message_callback(ch, method, properties, body):
+    message = " [x] recieved %s \n" % body.decode("utf-8")
+    if out_file is None:
+      print(message)
+    else:
+      out_file.write(message)
+  
+  return on_message_callback
+
+
+def start_consuming(ch, queue, callback, auto_ack):
+  ch.basic_consume(queue=queue, on_message_callback=callback, auto_ack=auto_ack)
+  print(" [*] Waiting for messages in queue %s. To exit press CTRL+C" % queue)
+  try:
+    ch.start_consuming()
+  except KeyboardInterrupt:
+    print(" [x] Closing connection... ")
+
+
+def run_listener(params, queue_name, out_filename):
+  with pika.BlockingConnection(params) as connection:
+      with connection.channel() as channel:
+        with open(out_filename, "a") as out_file:
+          start_consuming(channel, queue_name, on_message(out_file), False)
+
+
+def main():
+  config = read_config()
+  credentials = pika.PlainCredentials(config["user"], config["pass"])
+  params = pika.ConnectionParameters(
+    host=config["addr"],
+    port=config["port"],
+    credentials=credentials
+  )
+
+  listeners = []
+  for queue in config["queues"]:
+    p = multiprocessing.Process(target=run_listener, args=(params, queue["name"], queue["out"]))
+    p.start()
+    listeners.append(p)
+  
+  for p in listeners:
+    p.join()
+
+
+if __name__ == "__main__":
+  try:
+    main()
+  except KeyboardInterrupt:
+    print(" [x] Finishing... ")
